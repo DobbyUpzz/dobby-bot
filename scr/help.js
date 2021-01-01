322ed6854806b83d47eedb677ccf0e80d598d71e
DobbyUpzz <76834931+DobbyUpzz@users.noreply.github.com>
2021-01-01 05:42:49 -0300

    Add files via upload

---
+++ scr/help.js
@@ -0,0 +1,119 @@
+const help = (prefix) => {
+	return  
+
+╠════════════════════
+║   √ 𝐁𝐄𝐌 𝐕𝐈𝐍𝐃𝐎(𝐀) 𝐀𝐎 𝐌𝐄𝐍𝐔 √
+╠════✪〘 FIGURINHA 〙✪═════
+║
+╠➣ COMANDO: ${prefix}sticker
+╠➣ DESC: Converter foto/gif/video em figurinha
+╠➣ USO: basta enviar o comando\n
+║
+╠➣ COMANDO: ${prefix}sticker nobg
+╠➣ DESC: transforma imagem em figurinha sem fundo
+╠➣ USO:  Enviar imagem com o comando\n
+║
+╠➣ COMANDO: ${prefix}toimg
+╠➣ DESC: Converte a figurinha em imagem
+╠➣USO: Responda a figurinha com o comando\n
+║
+╠➣ COMANDO: ${prefix}tsticker
+╠➣ DESC: Converte o que você digitar em Figurinha
+╠➣ USO: ${prefix}tsticker [seu texto]\n
+║
+╠═════✪〘 MEME 〙✪══════
+║
+╠➣ COMANDO: ${prefix}meme
+╠➣ DESC: Imagens aleatórias de meme [Inglês]
+╠➣ USO: Apenas envie o comando\n
+║
+╠➣ COMANDO: ${prefix}memeindo
+╠➣ DESC: Imagens aleatórias de meme [Indo]
+╠➣ USO: Apenas envie o comando\n
+║
+╠═════✪〘 OUTROS 〙✪═════  
+║
+╠➣ COMANDO: ${prefix}gtts
+╠➣ DESC: O bot responde com um áudio
+╠➣ USO: ${prefix}gtts [cc] [text]\n
+╠➣ EXEMPLO: ${prefix}gtts pt Olá\n
+║
+╠➣ COMANDO: ${prefix}loli
+╠➣ DESC: Imagens de loli
+╠➣ USO: Apenas envie o comando\n
+║
+╠➣ COMANDO: ${prefix}nsfwloli
+╠➣ DESC: Imagens de nsfw loli
+╠➣ USO: Apenas envie o comando\n
+║
+╠➣ COMANDO: ${prefix}url2img
+╠➣ DESC: Tirar screenshots da web
+╠➣ USO: ${prefix}url2img [tipo] [sua url]\n
+║
+╠➣ COMANDO: ${prefix}simi
+╠➣ DESC: Sua mensagem será respondida por simi
+╠➣ USO: ${prefix}simi [seu texto]\n
+║
+╠➣ COMANDO: ${prefix}ocr
+╠➣ DESC: Pegue o texto escrito na foto
+╠➣ USO: Envie sua imagem com o comando\n
+║
+╠➣ COMANDO: ${prefix}wait
+╠➣ DESC: Saiba sobre o anime [Que anime é]
+╠➣ USO: Envie imagem do anime com o comando\ n
+║
+╠➣ COMANDO: *${prefix}setprefix
+╠➣ DESC: Substitui o prefixo do bot
+╠➣ USO: ${prefix}setprefix [texto|opcional]\n
+╠➣ EXEMPLO: ${prefix}setprefix /
+╠➣ NOTA: Usado apenas pelo dono do bot\n
+║
+╠═════✪〘 GRUPO 〙✪═════
+║➣ *USADOS SOMENTE POR ADMIN'S*
+║
+╠➣ COMANDO: ${prefix}add
+╠➣ DESC: Adicionar membro ao grupo
+╠➣ USO: ${prefix}add 55xxxxxxxx\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║
+╠➣ COMANDO: ${prefix}kick
+╠➣ DESC: Banir membros do grupo
+╠➣ USO: ${prefix}kick @tag do membro\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║➣ OBS: Não faça o bot remover duas pessoas ao mesmo tempo!\n
+║
+╠➣ COMANDO: ${prefix}promote
+╠➣ DESC: Promove um membro a ser admin
+╠➣ USO: ${prefix}promote @tag do membro\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║
+╠➣ COMANDO: ${prefix}demote
+╠➣ DESC: Promove um admin a ser membro comum
+╠➣ USO: ${prefix}demote @tag do admin\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║
+╠➣ COMANDO: ${prefix}linkgroup
+╠➣ DESC: Obtenha o link do grupo
+╠➣ USO: Apenas envie o comando\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║
+╠➣ COMANDO: ${prefix}tagall
+╠➣ DESC: Marca todos os membros do grupo
+╠➣ USO: Apenas envie o comando
+╠➣ NOTA: Você precisa ser um admin!\n
+║
+╠➣ COMANDO: ${prefix}simih
+╠➣ DESC: Ative o modo simi no grupo
+╠➣ ATIVAR: ${prefix}simih 1 para ativar 
+╠➣ DESATIVAR: ${prefix}simih 0 para desativar 
+╠➣ NOTA: Você precisa ser um admin\n
+║
+╠════════════════════
+║ 🍁 𝐃𝐎𝐍𝐎 𝐃𝐎 𝐁𝐎𝐓: 𝐷𝑂𝐵𝐵𝑌♱᭄ 🍁
+║
+║    🍁Wa.me//19086234869🍁
+╚════════════════════`
+
+}
+
+exports.help = help
